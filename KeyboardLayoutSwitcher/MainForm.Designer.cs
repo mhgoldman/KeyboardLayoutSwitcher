bb8d6fda502d1b8c849291b7bb88307e3dcb95fd
mhgoldman <martin@mgoldman.com>
2016-05-06 13:23:55 -0700

    first commit, again

---
+++ KeyboardLayoutSwitcher/MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace KeyboardLayoutSwitcher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.keyboardLayoutCmb = new System.Windows.Forms.ComboBox();
+            this.setLayoutBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // keyboardLayoutCmb
+            // 
+            this.keyboardLayoutCmb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.keyboardLayoutCmb.FormattingEnabled = true;
+            this.keyboardLayoutCmb.Location = new System.Drawing.Point(13, 26);
+            this.keyboardLayoutCmb.Name = "keyboardLayoutCmb";
+            this.keyboardLayoutCmb.Size = new System.Drawing.Size(259, 21);
+            this.keyboardLayoutCmb.TabIndex = 0;
+            // 
+            // setLayoutBtn
+            // 
+            this.setLayoutBtn.Location = new System.Drawing.Point(80, 96);
+            this.setLayoutBtn.Name = "setLayoutBtn";
+            this.setLayoutBtn.Size = new System.Drawing.Size(115, 36);
+            this.setLayoutBtn.TabIndex = 1;
+            this.setLayoutBtn.Text = "Set Layout && Restart";
+            this.setLayoutBtn.UseVisualStyleBackColor = true;
+            this.setLayoutBtn.Click += new System.EventHandler(this.setLayoutBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select the desired keyboard layout:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "WARNING: Your VM(s) will be restarted when this setting is changed!";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 139);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.setLayoutBtn);
+            this.Controls.Add(this.keyboardLayoutCmb);
+            this.Name = "MainForm";
+            this.Text = "Keyboard Layout Switcher";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox keyboardLayoutCmb;
+        private System.Windows.Forms.Button setLayoutBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ KeyboardLayoutSwitcher/MainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Configuration;
+
+namespace KeyboardLayoutSwitcher
+{
+    public partial class MainForm : Form
+    {
+        public static readonly string METADATA_ENDPOINT = ConfigurationManager.AppSettings["metadata_endpoint"]; //"http://gw/skytap";
+        public static readonly string REST_ENDPOINT = ConfigurationManager.AppSettings["rest_endpoint"]; //"http://172.16.150.1:3000/api/participant_preferences";
+        public static readonly Dictionary<string, string> KEYBOARD_LAYOUTS = new Dictionary<string, string>
+        {
+            { "Dutch-Belgium", "nl-be" },
+            { "English (US)", "us" },
+            { "English (UK)", "uk" },
+            { "Finnish", "fi" },
+            { "French", "fr" },
+            { "French-Belgium", "fr-be" },
+            { "French-Switzerland", "fr-ch" },
+            { "German", "de" },
+            { "German-Switzerland", "de-ch" },
+            { "Icelandic", "is" },
+            { "Italian", "it" },
+            { "Japanese", "jp" },
+            { "Norwegian", "no" },
+            { "Polish", "pt" },
+            { "Spanish", "es" }
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            keyboardLayoutCmb.Items.AddRange(KEYBOARD_LAYOUTS.Keys.ToArray());
+        }
+
+        private void setLayoutBtn_Click(object sender, EventArgs e)
+        {
+            Go();
+        }
+
+        private string GetKeyboardLayoutCode() 
+        {
+            object selectedItem = keyboardLayoutCmb.SelectedItem;
+
+            return selectedItem == null ? null : KEYBOARD_LAYOUTS[selectedItem.ToString()];
+        }
+
+        private string GetMetadata()
+        {
+            System.Net.WebClient client = new System.Net.WebClient();
+            client.Headers.Add("content-type", "application/json");
+            return client.DownloadString(METADATA_ENDPOINT);
+        }
+
+        private string GetPublishedURLFromMetadata(string metadata)
+        {
+            Regex pattern = new Regex(@".*(?<publishedURL>https\:\/\/cloud\.skytap\.com\/vms\/[a-z0-9]+\/desktops)");
+            Match match = pattern.Match(metadata);
+            return match.Groups["publishedURL"].Value;
+        }
+
+        private string GetConfigIDFromMetadata(string metadata)
+        {
+            Regex pattern = new Regex(@".*https\:\/\/cloud\.skytap\.com\/configurations\/(?<configID>[0-9]+)\/");
+            Match match = pattern.Match(metadata);
+            return match.Groups["configID"].Value;
+        }
+
+        private void Go()
+        {
+            try
+            {
+                string metadata = GetMetadata();
+                string pubURL = GetPublishedURLFromMetadata(metadata);
+                string configID = GetConfigIDFromMetadata(metadata);
+                string keyboardLayoutCode = GetKeyboardLayoutCode();
+
+                if (String.IsNullOrEmpty(keyboardLayoutCode))
+                {
+                    return;
+                }
+
+                string requestBody = "{\"participant\":{\"keyboard_layout\":\"" +
+                    keyboardLayoutCode +
+                    "\", \"published_url\":\"" +
+                    pubURL +
+                    "\", \"skytap_configuration_skytap_id\":\"" +
+                    configID + 
+                    "\"}}";
+                System.Net.WebClient client = new System.Net.WebClient();
+                client.Headers.Add("content-type", "application/json");
+                string s = Encoding.ASCII.GetString(
+                    client.UploadData(
+                        REST_ENDPOINT,
+                        "PUT",
+                        Encoding.Default.GetBytes(requestBody)
+                    )
+                );
+
+                MessageBox.Show("The change has been submitted. Please stand by while your environment restarts.");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("An error occurred: " + e.Message + "\n\n" + e.StackTrace);
+            }
+        }
+    }
+}
